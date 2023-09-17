c5f58574f0d85476b349a36a17dbff4c842a68c7
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-09-18 02:13:45 +0530

    Add new grid generation functionality

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const container = document.getElementById('container');
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.setAttribute('class', 'square');
-    container.appendChild(gridSquare);
-    gridSquare.addEventListener('mouseover', hoverEffect);
+const btn = document.getElementById('button');
+btn.addEventListener("click", resetGrid);
+
+function createGrid(squaresPerSide = 16) {
+
+    if (squaresPerSide > 100) {
+        squaresPerSide = 100;
+        alert("The input exceeds the maximum limit. Setting to 100.");
+    }
+
+    container.innerHTML = '';
+    const squareSize = 640 / squaresPerSide;
+
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.style.width = `${squareSize}px`;
+        gridSquare.style.height = `${squareSize}px`;
+        gridSquare.setAttribute('class', 'square');
+        container.appendChild(gridSquare);
+        gridSquare.addEventListener('mouseover', hoverEffect);
+    }
 }
 
 function hoverEffect(event) {
-    event.target.setAttribute('class', 'hover');
+    event.target.style.backgroundColor = `aqua`;
     }
+
+function resetGrid(event) {
+        let squaresPerSide = window.prompt("Enter number of squares per side (Max allowed 100).");
+        createGrid(squaresPerSide);
+    }
+
+createGrid();
