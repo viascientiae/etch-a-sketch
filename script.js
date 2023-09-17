c5f5a23fa31f0f85f91aac6198eb9e831f2ddd71
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-09-18 01:23:44 +0530

    Set up hover effect

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ for (let i = 0; i < 16 * 16; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.setAttribute('class', 'square');
     container.appendChild(gridSquare);
+    gridSquare.addEventListener('mouseover', hoverEffect);
 }
+
+function hoverEffect(event) {
+    event.target.setAttribute('class', 'hover');
+    }
